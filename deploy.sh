da4a947b7b77f802d641c7e59d273a300ee0cf6b
chadcarlson <chad.carlson@platform.sh>
2022-06-21 15:23:59 -0400

    actually eliminate noise.

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,3 @@ set -e
 # Setup drush. 
 php ./drush/platformsh_generate_drush_yml.php
 
-# Run the Platform.sh demo, if enabled.
-# .platform-scripts/demo.sh
-
-# Drupal deployment tasks.
-drush -q -y cache-rebuild
-drush -q -y updatedb
-drush -q -y config-import
